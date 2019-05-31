2aa87e1ab86116da2a75f335bc4727abf1b83fcc
2 <a2@HS-Lib-Lab-19.local>
2019-05-31 09:10:32 -0700

    joystick motor thing

--- joystick.js
+++ joystick.js
@@ -1,7 +1,10 @@
-var five = require("johnny-five");
+var five = require("johnny-five"),
+        servo, board, joystick;
 var board = new five.Board();
 
 board.on("ready", function() {
+
+  servo = new five.Servo(10);
 
   // Create a new `joystick` hardware instance.
   var joystick = new five.Joystick({
@@ -9,10 +12,19 @@ board.on("ready", function() {
     pins: ["A0", "A1"]
   });
 
+  board.repl.inject({
+    joystick: joystick,
+    servo: servo
+    });
+
+
   joystick.on("change", function() {
     console.log("Joystick");
     console.log("  x : ", this.x);
     console.log("  y : ", this.y);
     console.log("--------------------------------------");
   });
+  if (this.y < 0){
+    servo.sweep();
+  }
 });
